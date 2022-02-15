8ed9728f9840968fc6df6943ef3805c9be7a729e
odysseia06 <91327287+odysseia06@users.noreply.github.com>
2022-02-15 10:06:03 +0300

    Add files via upload

--- socket/RPi_socket/server.py
+++ socket/RPi_socket/server.py
@@ -5,10 +5,26 @@ HOST = socket.gethostname()
 print(HOST)
 PORT = 12345
 s.bind((HOST, PORT))
+s.settimeout(1.0)
 s.listen()
 
-while True:
-    conn, addr = s.accept()
-    print("Got connection from", addr)
-    conn.send("Thank you for connecting.")
-    conn.close()
+
+
+try:
+    while True:
+        try:
+
+                conn, addr = s.accept()
+                print("Got connection from", addr)
+                message = "Thank you for connecting."
+                conn.send(message.encode())
+                conn.close()
+        except socket.timeout:
+            pass
+        except KeyboardInterrupt:
+            pass
+except KeyboardInterrupt:
+    s.shutdown
+    s.close()
+    print("Server closed with KeyboardInterrupt!")
+
